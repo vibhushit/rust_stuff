c0231a53c25cc222bbe478e0b9a9e9e2558c7f8e
vibhushit <vibhushit10@gmail.com>
2024-04-19 11:52:11 +0530

    Add submodules

---
+++ hello_world/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    println!("Hello, world");
+}
+
+// println! is a macro as we are use ! after the name and we passed "Hello, world" as the argument
+// ot the macro. So println! is not a function it is different from a function and we use "!" to
+// denote that.
+// main() fn is the entry point to the rust program.
